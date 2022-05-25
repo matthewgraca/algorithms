1016ed09decedcbe236b5630d2f77b455d4b1e7e
matthewgraca <mgraca@cpp.edu>
2022-05-25 14:32:53 -0700

    added sorting boilerplate and wrote tests

---
+++ src/main/java/com/mgraca/algorithms/sorting/Quick.java
@@ -0,0 +1,31 @@
+package com.mgraca.algorithms.sorting;
+
+public class Quick{
+  /**
+   * Sorts the contents of a given array
+   * @param a The array being sorted
+   */
+  public static void sort(Comparable[] a){
+
+  }
+
+  /**
+   * Checks if one item is smaller than another
+   * @param v An item being checked
+   * @param w The other item being checked against
+   * @return  True if v is smaller than w
+   */
+  private static boolean less(Comparable v, Comparable w){
+    return true;
+  }
+
+  /**
+   * Swaps the contents of two elements in an array
+   * @param a The array containing the elements to be swapped
+   * @param i The element whose data will be swapped with j
+   * @param j The element whose data will be swapped with i
+   */
+  private static void exchange(Comparable[] a, int i, int j){
+
+  }
+}
---
+++ src/test/java/com/mgraca/algorithms/sorting/QuickTest.java
@@ -0,0 +1,86 @@
+package com.mgraca.algorithms.sorting;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+import java.util.Random;
+import java.util.Arrays;
+
+public class QuickTest{
+  @Test
+  public void alreadySorted(){
+    Integer[] actual = new Integer[1000];
+    Integer[] expected = new Integer[1000];
+    for (int i = 0; i < actual.length; i++){
+      actual[i] = expected[i] = i;
+    }
+    Arrays.sort(expected);
+    Insertion.sort(actual);
+    assertTrue(Arrays.equals(actual, expected));
+  }
+
+  @Test
+  public void reverseSorted(){
+    Integer[] actual = new Integer[1000];
+    Integer[] expected = new Integer[1000];
+    int n = actual.length;
+    for (int i = 0; i < n; i++){
+      actual[n-i-1] = expected[n-i-1] = i;
+    }
+    Arrays.sort(expected);
+    Insertion.sort(actual);
+    assertTrue(Arrays.equals(actual, expected));
+  }
+
+  @Test
+  public void randomListA(){
+    Random rng = new Random(69420);
+    Integer[] actual = new Integer[1000];
+    Integer[] expected = new Integer[1000];
+    for (int i = 0; i < actual.length; i++){
+      actual[i] = expected[i] = rng.nextInt();
+    }
+    Arrays.sort(expected);
+    Insertion.sort(actual);
+    assertTrue(Arrays.equals(actual, expected));
+  }
+
+  @Test
+  public void randomListB(){
+    Random rng = new Random(11111);
+    Integer[] actual = new Integer[1000];
+    Integer[] expected = new Integer[1000];
+    for (int i = 0; i < actual.length; i++){
+      actual[i] = expected[i] = rng.nextInt();
+    }
+    Arrays.sort(expected);
+    Insertion.sort(actual);
+    assertTrue(Arrays.equals(actual, expected));
+  }
+
+  @Test
+  public void randomListC(){
+    Random rng = new Random(49051);
+    Integer[] actual = new Integer[1000];
+    Integer[] expected = new Integer[1000];
+    for (int i = 0; i < actual.length; i++){
+      actual[i] = expected[i] = rng.nextInt();
+    }
+    Arrays.sort(expected);
+    Insertion.sort(actual);
+    assertTrue(Arrays.equals(actual, expected));
+  }
+
+  @Test
+  public void randomListD(){
+    Random rng = new Random(57840);
+    Integer[] actual = new Integer[1000];
+    Integer[] expected = new Integer[1000];
+    for (int i = 0; i < actual.length; i++){
+      actual[i] = expected[i] = rng.nextInt();
+    }
+    Arrays.sort(expected);
+    Insertion.sort(actual);
+    assertTrue(Arrays.equals(actual, expected));
+  }
+}
