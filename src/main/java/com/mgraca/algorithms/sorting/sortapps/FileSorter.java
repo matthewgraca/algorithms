5678068f16a675cab9e43f776a61d5a5ef42e7f3
matthewgraca <mgraca@cpp.edu>
2022-06-15 13:29:36 -0700

    added file sorting app

--- src/main/java/com/mgraca/algorithms/sorting/sortapps/FileSorter.java
+++ src/main/java/com/mgraca/algorithms/sorting/sortapps/FileSorter.java
@@ -1,11 +1,33 @@
 package com.mgraca.algorithms.sorting.sortapps;
 
+import java.io.File;
+import java.util.Arrays;
+
 public class FileSorter{
   /**
    * Prints files in a given directory, sorted by file name
    */
   public static void main(String[] args)
   {
+    File directory = new File(args[0]);
+    if (!directory.exists()){
+      System.out.println(args[0] + " does not exist.");
+      return;
+    }
+    if (!directory.isDirectory()){
+      System.out.println(args[0] + " is not a directory.");
+      return;
+    }
 
+    File[] files = directory.listFiles();
+    if (files == null){
+      System.out.println("Could not read files.");
+      return;
+    }
+
+    Arrays.sort(files);
+    for (File i : files){
+      System.out.println(i.getName());
+    }
   }
 }
