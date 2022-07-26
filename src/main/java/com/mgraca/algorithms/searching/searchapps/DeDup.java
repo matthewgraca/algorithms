3bbb384fd0a8ae8acbbadf85df8556b4cc4e45b5
matthewgraca <mgraca@cpp.edu>
2022-07-26 13:27:15 -0700

    updated comment

--- src/main/java/com/mgraca/algorithms/searching/searchapps/DeDup.java
+++ src/main/java/com/mgraca/algorithms/searching/searchapps/DeDup.java
@@ -5,6 +5,10 @@ package com.mgraca.algorithms.searching.searchapps;
  * Wherein we read a stream of inputs, remove all duplicate keys, and print 
  * the result. We read one string at a time, so memory requirements are light.
  * The result is a string of all the distinct keys from the standard input.
+ * 
+ * While it would be preferrable to not use files and properly test the output,
+ * a key point of this exercise is light memory requirements, using one string 
+ * at a time.
  */
 
 import java.io.File;
