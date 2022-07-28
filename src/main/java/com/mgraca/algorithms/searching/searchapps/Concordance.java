de10d876f2dff26baf9e5cbd016ef316a44889e5
matthewgraca <mgraca@cpp.edu>
2022-07-28 13:01:45 -0700

    reimplemented concordance app to match specification for context

--- src/main/java/com/mgraca/algorithms/searching/searchapps/Concordance.java
+++ src/main/java/com/mgraca/algorithms/searching/searchapps/Concordance.java
@@ -18,17 +18,20 @@ import java.util.ArrayList;
 
 public class Concordance{
   public static void main(String[] args){
+    final int CONTEXT = 5;
     try{
       // read strings from file
       Scanner sc = new Scanner(new File(args[0]));
       Scanner in = new Scanner(System.in);
 
       // associate each string with an integer, counting up
-      int count = 1;
+      int count = 0;
       SeparateChainingHashST<String, ArrayList<Integer>> st = new SeparateChainingHashST<>();
+      ArrayList<String> words = new ArrayList<>();
       while (sc.hasNext()){
         // ignore punctuation and case
         String word = sc.next().toLowerCase().replaceAll("[\\W]", "");
+        words.add(word);
         // add the word-integer pair, or just add the integer to an existing word 
         if (!st.contains(word))
           st.put(word, new ArrayList<Integer>());
@@ -41,8 +44,22 @@ public class Concordance{
       in = new Scanner(System.in);
       while (in.hasNextLine()){
         String query = in.next();
-        if (st.contains(query))
-          System.out.println(st.get(query).toString());
+        ArrayList<Integer> indecies = st.get(query);
+        // print out the context of the query word
+        if (indecies != null){
+          // iterate through each word that appears 
+          for (int k = 0; k < indecies.size(); k++){
+            int j = indecies.get(k);
+            // print left context; if left context < actual context, start from 0
+            for (int i = Math.max(0, j-CONTEXT+1); i < j; i++)
+              System.out.print(words.get(i) + " ");
+            System.out.print("**" + words.get(j) + "** ");
+            // print right context; if right context > actual context, end at end
+            for (int i = j+1; i < Math.min(j+CONTEXT, words.size()); i++)
+              System.out.print(words.get(i) + " ");
+            System.out.println();
+          }
+        }
       }
       System.out.println();
       in.close();
