c6af0e2f3684143ee256635d7e5557feed8e385d
matthewgraca <mgraca@cpp.edu>
2022-08-03 15:42:04 -0700

    added undirected graph implementation and tests

--- src/main/java/com/mgraca/algorithms/graphs/undirected/Graph.java
+++ src/main/java/com/mgraca/algorithms/graphs/undirected/Graph.java
@@ -2,27 +2,86 @@ package com.mgraca.algorithms.graphs.undirected;
 
 import com.mgraca.algorithms.fundamentals.Bag;
 import java.util.Scanner;
+import java.util.NoSuchElementException;
 
 /**
  * An implementation of an undirected graph using an adjacency list, 
  * where each list is a multiset (bag)
  */
 public class Graph{
+  private int V;
+  private int E;
+  private Bag<Integer>[] adj;
 
   /**
    * Initializes a graph with V vertices
    * @param V the number of vertices
+   * @throws IllegalArgumentException if the number of vertices is negative
    */
   public Graph(int V){
-
+    if (V < 0){
+      String msg = "Cannot initialize a graph with negative vertices";
+      throw new IllegalArgumentException(msg);
+    }
+    this.V = V;
+    this.E = 0;
+    @SuppressWarnings("unchecked")
+    Bag<Integer>[] temp = (Bag<Integer>[]) new Bag[V];
+    adj = temp;
+    for (int v = 0; v < V; v++)
+      adj[v] = new Bag<Integer>();
   }
 
   /**
    * Initializes a graph using standard input
    * @param in standard input stream
+   * @throws IllegalArgumentException if the argument is null
+   * @throws IllegalArgumentException if the read vertex or edge is negative
+   * @throws IllegalArgumentException if the edge endpoint is out of range
+   * @throws IllegalArgumentException if the input is in the wrong format
    */
   public Graph(Scanner in){
+    if (in == null)
+      throw new IllegalArgumentException("Argument is null");
+    try{
+      // read vertex number
+      checkNextInt(in);
+      this.V = in.nextInt();
+      if (V < 0){
+        String msg = "Cannot initialize a graph with negative vertices";
+        throw new IllegalArgumentException(msg);
+      }
+      // create adjacency list
+      @SuppressWarnings("unchecked")
+      Bag<Integer>[] temp = (Bag<Integer>[]) new Bag[V];
+      adj = temp;
+      for (int v = 0; v < V; v++)
+        adj[v] = new Bag<Integer>();
 
+      // initialize bags w/in the adj list
+      checkNextInt(in);
+      int E = in.nextInt();
+      if (E < 0){
+        String msg = "Cannot initialize a graph with negative edges";
+        throw new IllegalArgumentException(msg);
+      }
+      for (int i = 0; i < E; i++){
+        checkNextInt(in);
+        int v = in.nextInt();
+        checkNextInt(in);
+        int w = in.nextInt();
+        addEdge(v, w);
+      }
+    }
+    catch (NoSuchElementException e){
+      throw new IllegalArgumentException("Input is in the wrong format");
+    }
+  }
+
+  // throws IllegalArgumentException if an int can't be read from std in
+  private void checkNextInt(Scanner in){
+    if (!in.hasNextInt())
+      throw new IllegalArgumentException("Wrong format");
   }
 
   /**
@@ -30,7 +89,7 @@ public class Graph{
    * @return the number of vertices in the graph
    */
   public int V(){
-    return 0;
+    return V;
   }
 
   /**
@@ -38,16 +97,21 @@ public class Graph{
    * @return the number of edges in the graph
    */
   public int E(){
-    return 0;
+    return E;
   }
 
   /**
    * Addes edge v-w to this graph
    * @param v a vertex
    * @param e the other vertex 
+   * @throws IllegalArgumentException if any vertex is out of range
    */
   public void addEdge(int v, int w){
-
+    validateVertex(v);
+    validateVertex(w);
+    E++;
+    adj[v].add(w);
+    adj[w].add(v);
   }
 
   /**
@@ -56,21 +120,25 @@ public class Graph{
    * @return the degree of v
    */
   public int degree(int v){
-    return 0;
+    validateVertex(v);
+    return adj[v].size();
   }
 
   // throws IllegalArgumentException if the vertex v is not in [0, V-1]
   private void validateVertex(int v){
-
+    if (v < 0 || v > V-1)
+      throw new IllegalArgumentException("Vertex must be [0, " + (V-1) + "]");
   }
 
   /**
    * Gets the vertices directly adjacent to vertex v
    * @param v the vertex
    * @return the vertices adjacent to v as an iterable
+   * @throws IllegalArgumentException if the vertex is out of range
    */
   public Iterable<Integer> adj(int v){
-    return null;
+    validateVertex(v);
+    return adj[v];
   }
 
   /**
@@ -79,7 +147,14 @@ public class Graph{
    *          by the adjacency lists
    */
   public String toString(){
-    return null;
+    String s = V + " vertices, " + E + " edges\n";
+    for (int v = 0; v < V; v++){
+      s += v + ": ";
+      for (int w : this.adj(v))
+        s += w + " ";
+      s += "\n";
+    }
+    return s;
   }
 
 }
--- src/test/java/com/mgraca/algorithms/graphs/undirected/GraphTest.java
+++ src/test/java/com/mgraca/algorithms/graphs/undirected/GraphTest.java
@@ -2,25 +2,228 @@ package com.mgraca.algorithms.graphs.undirected;
 
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class GraphTest{
   /****************************************************************************
-   * Constructor Tests
+   * Int Constructor Tests
    ***************************************************************************/
   
+  @Test(expected=IllegalArgumentException.class)
+  public void constructorWithNegativeVerticesThrowsException(){
+    Graph g = new Graph(-1);
+  }
+
+  @Test
+  public void constructorInitializesGraph(){
+    Graph g = new Graph(10);
+    assertTrue(g.V() == 10);
+  }
+
   /****************************************************************************
-   * Adjacency Tests
+   * Scanner constructor tests
    ***************************************************************************/
+
+  @Test(expected=IllegalArgumentException.class)
+  public void nullArgumentThrowsException(){
+    Graph g = new Graph(null);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void readVertexIsNegativeThrowsException(){
+    try{
+      File path = new File("data/graphTest/tinyGBadVertex.txt");
+      Scanner in = new Scanner(path);
+      Graph g = new Graph(in);
+      in.close();
+    }
+    catch (FileNotFoundException e){
+      e.printStackTrace();
+    }
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void readEdgeIsNegativeThrowsException(){
+    try{
+      File path = new File("data/graphTest/tinyGBadEdge.txt");
+      Scanner in = new Scanner(path);
+      Graph g = new Graph(in);
+      in.close();
+    }
+    catch (FileNotFoundException e){
+      e.printStackTrace();
+    }
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void readAddEdgeIsOutOfRangeThrowsException(){
+    try{
+      File path = new File("data/graphTest/tinyGBadAddEdge.txt");
+      Scanner in = new Scanner(path);
+      Graph g = new Graph(in);
+      in.close();
+    }
+    catch (FileNotFoundException e){
+      e.printStackTrace();
+    }
+  }
+  
+  @Test(expected=IllegalArgumentException.class)
+  public void badFormatThrowsException(){
+    try{
+      File path = new File("data/graphTest/tinyGBadInputFormat.txt");
+      Scanner in = new Scanner(path);
+      Graph g = new Graph(in);
+      in.close();
+    }
+    catch (FileNotFoundException e){
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  public void constructorAcceptsWellFormattedFileWithGoodInputs(){
+    try{
+      File path = new File("data/graphTest/tinyG.txt");
+      Scanner in = new Scanner(path);
+      Graph g = new Graph(in);
+      in.close();
+      assertTrue(g.V() == 13 && g.E() == 13);
+    }
+    catch (FileNotFoundException e){
+      e.printStackTrace();
+    }
+  }
 
   /****************************************************************************
    * Add edge Tests
    ***************************************************************************/
 
+  @Test(expected=IllegalArgumentException.class)
+  public void addEdgeThrowsExceptionForVertexAOutOfRange(){
+    Graph g = new Graph(4);
+    g.addEdge(5, 0);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void addEdgeThrowsExceptionForVertexBOutOfRange(){
+    Graph g = new Graph(4);
+    g.addEdge(0, 5);
+  }
+
+  @Test
+  public void addingValidEdgeAddsIt(){
+    Graph g = new Graph(4);
+    g.addEdge(0,3);
+    assertTrue(g.E() == 1);
+  }
+
+  /****************************************************************************
+   * Adjacency Tests
+   ***************************************************************************/
+
+  @Test(expected=IllegalArgumentException.class)
+  public void adjacentUnderRangeVertexThrowsException(){
+    Graph g = new Graph(4);
+    g.addEdge(0,3);
+    g.adj(-1);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void adjacentOverRangeVertexThrowsException(){
+    Graph g = new Graph(4);
+    g.addEdge(0,3);
+    g.adj(5);
+  }
+
+  @Test
+  public void adjacentOfEmptyGraphWithinRangeOutputsCorrectly(){
+    Graph g = new Graph(4);
+    for (Integer i : g.adj(3))
+      assertTrue(false);
+    assertTrue(true);
+  }
+
+  @Test
+  public void adjacentOfFilledGraphWithinRangeOutputsCorrectly(){
+    Graph g = new Graph(5);
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(0,4);
+    g.addEdge(3,4);
+    int j = 4;
+    for (Integer i : g.adj(0)){
+      assertTrue(i == j);
+      j /= 2;
+    }
+  }
+
   /****************************************************************************
    * Degree Tests
    ***************************************************************************/
 
+  @Test(expected=IllegalArgumentException.class)
+  public void degreeUnderRangeVertexThrowsException(){
+    Graph g = new Graph(4);
+    g.degree(-1);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void degreeOverRangeVertexThrowsException(){
+    Graph g = new Graph(4);
+    g.degree(5);
+  }
+
+  @Test
+  public void degreeOfEmptyGraphOutputsCorrectly(){
+    Graph g = new Graph(4);
+    assertTrue(g.degree(0) == 0);
+  }
+
+  @Test
+  public void degreeOfFilledGraphWithinRangeOutputsCorrectly(){
+    Graph g = new Graph(5);
+    g.addEdge(0,1);
+    g.addEdge(0,2);
+    g.addEdge(0,4);
+    g.addEdge(3,4);
+    assertTrue(g.degree(0) == 3);
+  }
+
   /****************************************************************************
    * To String Tests
    ***************************************************************************/
+
+  @Test
+  public void toStringTest(){
+    Graph g = null;
+    try{
+      File path = new File("data/graphTest/tinyG.txt");
+      Scanner in = new Scanner(path);
+      g = new Graph(in);
+      in.close();
+      assertTrue(g.V() == 13 && g.E() == 13);
+    }
+    catch (FileNotFoundException e){
+      e.printStackTrace();
+    }
+    String gString = g.V() + " vertices, " + g.E() + " edges\n";
+    gString += "0: 6 2 1 5 \n";
+    gString += "1: 0 \n";
+    gString += "2: 0 \n";
+    gString += "3: 5 4 \n";
+    gString += "4: 5 6 3 \n";
+    gString += "5: 3 4 0 \n";
+    gString += "6: 0 4 \n";
+    gString += "7: 8 \n";
+    gString += "8: 7 \n";
+    gString += "9: 11 10 12 \n";
+    gString += "10: 9 \n";
+    gString += "11: 9 12 \n";
+    gString += "12: 11 9 \n";
+    assertTrue( "\nExpected:\n" + gString + "\nReturned:\n" + g.toString(), 
+                gString.equals(g.toString()));
+  }
 }
