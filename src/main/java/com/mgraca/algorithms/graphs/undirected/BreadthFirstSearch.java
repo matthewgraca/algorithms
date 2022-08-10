5f9a0f27e59fa1dbdd3c8a4ec06446a9a857cbc1
matthewgraca <mgraca@cpp.edu>
2022-08-10 14:05:50 -0700

    added breadth first search implementation and tests

---
+++ src/main/java/com/mgraca/algorithms/graphs/undirected/BreadthFirstSearch.java
@@ -0,0 +1,78 @@
+package com.mgraca.algorithms.graphs.undirected;
+
+import com.mgraca.algorithms.fundamentals.LinkedQueue;
+import com.mgraca.algorithms.fundamentals.LinkedStack;
+
+public class BreadthFirstSearch{
+  private boolean[] marked;
+  private int[] edgeTo;
+  private final int s;
+
+  /**
+   * Initializes and executes breadth-first search
+   * @param g the graph
+   * @param s the source vertex
+   * @throws IllegalArgumentException if the source is out of range
+   */
+  public BreadthFirstSearch(Graph g, int s){
+    marked = new boolean[g.V()];
+    edgeTo = new int[g.V()];
+    validateVertex(s);
+    this.s = s;
+    bfs(g, s);
+  }
+
+  // uses bfs to find shortest paths connected to s
+  private void bfs(Graph g, int s){
+    validateVertex(s);
+    LinkedQueue<Integer> queue = new LinkedQueue<>();
+    marked[s] = true;
+    queue.enqueue(s);
+    while (!queue.isEmpty()){
+      int v = queue.dequeue();
+      for (int w : g.adj(v)){
+        if (!marked[w]){
+          edgeTo[w] = v;
+          marked[w] = true;
+          queue.enqueue(w);
+        }
+      }
+    }
+  }
+
+  /**
+   * Checks if v has been visited (if a path exists between source and v)
+   * @param v the vertex
+   * @return true if a given vertex was visited, false if not
+   * @throws IllegalArgumentException if the vertex is out of range
+   */
+  public boolean hasPathTo(int v){
+    validateVertex(v);
+    return marked[v];
+  }
+
+  // ensure v is within range
+  private void validateVertex(int v){
+    int V = marked.length;
+    if (v < 0 || v >= V)
+      throw new IllegalArgumentException("vertex must be [0,"+ V + ")");
+  }
+
+  /**
+   * Creates an iterable showing the shortest path from the source to v
+   * @param v the vertex
+   * @return an iterable of the path to v from the source in bfs visit order;
+   * null if no such path exists
+   * @throws IllegalArgumentException if the vertex if out of range
+   */
+  public Iterable<Integer> pathTo(int v){
+    validateVertex(v);
+    if (!hasPathTo(v))
+      return null;
+    LinkedStack<Integer> path = new LinkedStack<>();
+    for (int x = v; x != s; x = edgeTo[x])
+      path.push(x);
+    path.push(s);
+    return path;
+  }
+}
---
+++ src/test/java/com/mgraca/algorithms/graphs/undirected/BreadthFirstSearchTest.java
@@ -0,0 +1,86 @@
+package com.mgraca.algorithms.graphs.undirected;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
+public class BreadthFirstSearchTest{
+  @Test(expected=IllegalArgumentException.class)
+  public void vertexBelowRangeThrowsException(){
+    Graph g = new Graph(3);
+    BreadthFirstSearch bfs = new BreadthFirstSearch(g, -1);
+  }
+
+  @Test(expected=IllegalArgumentException.class)
+  public void vertexAboveRangeThrowsException(){
+    Graph g = new Graph(3);
+    BreadthFirstSearch bfs = new BreadthFirstSearch(g, 3);
+  }
+
+  @Test
+  public void properVisitOrderOnABalancedTree(){
+    int[] expected = {0, 1, 4, 9};
+    Graph g = new Graph(10); 
+    g.addEdge(0, 2);
+    g.addEdge(0, 1);
+    g.addEdge(1, 4);
+    g.addEdge(1, 3);
+    g.addEdge(2, 6);
+    g.addEdge(2, 5);
+    g.addEdge(3, 8);
+    g.addEdge(3, 7);
+    g.addEdge(4, 9);
+    BreadthFirstSearch bfs = new BreadthFirstSearch(g, 0);
+    int i = 0;
+    for (int v : bfs.pathTo(9)){
+      assertTrue( "Expected " + expected[i] + ", returned " + v, 
+                  v == expected[i]);
+      i++;
+    }
+  }
+  
+  @Test
+  public void properVisitOrderOnUnbalancedTree(){
+    int[] expected = {0, 9};
+    Graph g = new Graph(10);
+    g.addEdge(0, 9);
+    g.addEdge(0, 1);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+    g.addEdge(3, 4);
+    g.addEdge(4, 5);
+    g.addEdge(5, 6);
+    g.addEdge(6, 7);
+    g.addEdge(7, 8);
+    BreadthFirstSearch bfs = new BreadthFirstSearch(g, 0);
+    int i = 0;
+    for (int v : bfs.pathTo(9)){
+      assertTrue( "Expected " + expected[i] + ", returned " + v, 
+                  v == expected[i]);
+      i++;
+    }
+  }
+
+  @Test
+  public void properVisitOrderOnAGeneralGraph(){
+    int[] expected = {0, 2, 3};
+    try{
+      Graph g = new Graph(new Scanner(new File("data/graphTest/tinyCG.txt")));
+      BreadthFirstSearch bfs = new BreadthFirstSearch(g, 0);
+      int i = 0;
+      for (int v : bfs.pathTo(3)){
+        assertTrue( "Expected " + expected[i] + ", returned " + v, 
+                    v == expected[i]);
+        i++;
+      }
+    }
+    catch (FileNotFoundException e){
+      e.printStackTrace();
+    }
+  }
+
+}
